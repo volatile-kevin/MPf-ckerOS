aebd28ffd3299936054cb3b57c4817e1966b7335
Ao <dao3@illinois.edu>
2019-10-21 03:16:13 -0500

    fixed

--- student-distrib/paging.h
+++ student-distrib/paging.h
@@ -1,6 +1,4 @@
-#ifndef PAGING_H_INCLUDED
-#define PAGING_H_INCLUDED
-
+#include "paging.h"
 #include "types.h"
 
 //MACROS
@@ -9,15 +7,12 @@
 #define INITIAL_PAGEDIR_VAL 0x00000002
 #define INITIAL_PAGETABLE_ATTRIBUTE 3
 
-void init_paging(void);
-//void* get_physaddr(void* virtualaddr);
-//void* map_page(void* physaddr, void* virtualaddr, unsigned int flags);
-extern void flush_tlb(void);
-extern void enable_paging(void);
+//Paging arrays
+extern uint32_t pageDirectory[ONE_KB] __attribute__((aligned(FOUR_KB)));
+extern uint32_t pageTable[ONE_KB] __attribute__((aligned(FOUR_KB)));
 
-////Paging arrays
-extern uint32_t page_directory[ONE_KB] __attribute__((aligned(FOUR_KB)));
-extern uint32_t page_table[ONE_KB] __attribute__((aligned(FOUR_KB)));
+void init_paging();
+void* get_physaddr(void* virtualaddr);
+void* map_page(void* physaddr, void* virtualaddr, unsigned int flags);
+void flush_tlb();
 
-
-#endif
