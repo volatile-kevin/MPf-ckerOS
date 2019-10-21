e3a3e7e4d427da63ed60e721ab4302a24f6ba4e9
Daniel Ao <dao3@illinois.edu>
2019-10-21 12:06:42 -0500

    finished cp1 paging

--- student-distrib/paging.c
+++ student-distrib/paging.c
@@ -28,10 +28,17 @@ void init_paging(void){
         //   Supervisor: Only kernel-mode can access them
         //   Write Enabled: It can be both read from and written to
         //   Not Present: The page is present
-        page_table[i] = (i*0x1000) | INITIAL_PAGETABLE_ATTRIBUTE;
+        page_table[i] = (i*FOUR_KB) | INITIAL_PAGETABLE_ATTRIBUTE;
     }
+    //This sets the entry in the page table for video memory
+    page_table[VIDEO_MEM] = page_table[VIDEO_MEM] | INITIAL_VIDEOMEM_ATTRIBUTE;
+
+
     //Add the first page table to the page directory
     page_directory[0] = ((unsigned int)page_table) | INITIAL_PAGETABLE_ATTRIBUTE;
+    //The second page is the page for the kernel. We map this to 4MB since the doc told us to
+    page_directory[1] = FOUR_MB | INITIAL_KERNEL_ATTRIBUTE;
+
 
     //We have to use inline assembly for this part because we cannot access CR3 in C
     enable_paging();
@@ -65,26 +72,28 @@ void init_paging(void){
  * returns: The physical address
  * maps the virtual address to the physical address
  */
-//void* map_page(void* physaddr, void* virtualaddr, unsigned int flags){
-//    // Make sure that both addresses are page-aligned.
-//
-//    unsigned long pdindex = (unsigned long)virtualaddr >> 22;
-//    unsigned long ptindex = (unsigned long)virtualaddr >> 12 & 0x03FF;
-//
-//    unsigned long * pd = (unsigned long *)0xFFFFF000;
-//    // Here you need to check whether the PD entry is present.
-//    // When it is not present, you need to create a new empty PT and
-//    // adjust the PDE accordingly.
-//
-//    unsigned long * pt = ((unsigned long *)0xFFC00000) + (0x400 * pdindex);
-//    // Here you need to check whether the PT entry is present.
-//    // When it is, then there is already a mapping present. What do you do now?
-//
-//    pt[ptindex] = ((unsigned long)physaddr) | (flags & 0xFFF) | 0x01; // Present
-//
-//    // Now you need to flush the entry in the TLB
-//    // or you might not notice the change.
-//    flush_tlb();
-//}
+void map_page(void* physaddr, void* virtualaddr, unsigned int flags){
+    // Make sure that both addresses are page-aligned.
+    if(!((unsigned long)physaddr%FOUR_KB) && !((unsigned long)virtualaddr%FOUR_KB)){
+        return;
+    }
+    unsigned long pdindex = (unsigned long)virtualaddr >> 22;
+    unsigned long ptindex = (unsigned long)virtualaddr >> 12 & 0x03FF;
+
+    unsigned long * pd = (unsigned long *)0xFFFFF000;
+    // Here you need to check whether the PD entry is present.
+    // When it is not present, you need to create a new empty PT and
+    // adjust the PDE accordingly.
+
+    unsigned long * pt = ((unsigned long *)0xFFC00000) + (0x400 * pdindex);
+    // Here you need to check whether the PT entry is present.
+    // When it is, then there is already a mapping present. What do you do now?
+
+    pt[ptindex] = ((unsigned long)physaddr) | (flags & 0xFFF) | 0x01; // Present
+
+    // Now you need to flush the entry in the TLB
+    // or you might not notice the change.
+    flush_tlb();
+}
 
 
--- student-distrib/paging.h
+++ student-distrib/paging.h
@@ -5,13 +5,17 @@
 
 //MACROS
 #define ONE_KB 1024
-#define FOUR_KB 4096
+#define FOUR_KB 4096 //Default size of a page
+#define FOUR_MB 4194304 //Size of a big page
 #define INITIAL_PAGEDIR_VAL 0x00000002
-#define INITIAL_PAGETABLE_ATTRIBUTE 3
+#define INITIAL_PAGETABLE_ATTRIBUTE 0x2
+#define INITIAL_VIDEOMEM_ATTRIBUTE 0x3
+#define INITIAL_KERNEL_ATTRIBUTE 0x83 //Sets the size to 1 so the page is 4MB
+#define VIDEO_MEM       0xB8000 //defined in lib.c
 
 void init_paging(void);
 //void* get_physaddr(void* virtualaddr);
-//void* map_page(void* physaddr, void* virtualaddr, unsigned int flags);
+void map_page(void* physaddr, void* virtualaddr, unsigned int flags);
 extern void flush_tlb(void);
 extern void enable_paging(void);
 
