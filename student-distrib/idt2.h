20ab35f3b22f7763656085a1b309be19a5ba388f
joseal2 <joseal2@illinois.edu>
2019-10-21 10:25:05 -0500

    Upload New File

---
+++ student-distrib/idt2.h
@@ -0,0 +1,23 @@
+void de();
+void db();
+void nmi();
+void bp();
+void of();
+void br();
+void ud();
+void nm();
+void df();
+void cpso();
+void ts();
+void np();
+void ssf();
+void gp();
+void pf();
+void ir();
+void mf();
+void ac();
+void mc();
+void xf();
+void keyboard();
+
+void setup_idt_inplace();
