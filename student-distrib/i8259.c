106f46e5f86717ac4362f8e24089e167768cec78
joseal2 <joseal2@illinois.edu>
2019-10-21 17:25:47 -0500

    Replace i8259.c

--- student-distrib/i8259.c
+++ student-distrib/i8259.c
@@ -12,24 +12,24 @@
 #define PIC1_DATA	(PIC1+1)
 #define PIC2_COMMAND	PIC2
 #define PIC2_DATA	(PIC2+1)
-
-
+#define SIZEOFPIC1 8
+#define MSLINK 2
+#define MMASK 0xFB
+#define SMASK 0xFF
 /* Interrupt masks to determine which interrupts are enabled and disabled */
-uint8_t master_mask = 0xFF; /* IRQs 0-7  */
-uint8_t slave_mask = 0xFF;  /* IRQs 8-15 */
+uint8_t master_mask = MMASK; /* IRQs 0-7  */
+uint8_t slave_mask = SMASK;  /* IRQs 8-15 */
 
 /* Initialize the 8259 PIC */
 void i8259_init(void) {
-  // unsigned char mask1, mask2;
-  // save masks
-  // master_mask = inb(PIC1_DATA);
-  // slave_mask = inb(PIC2_DATA);
+
   // mask the master
   outb(master_mask, PIC1_DATA);
   // mask the slave
   outb(slave_mask, PIC2_DATA);
 
   // starts initialization sequence in casade mode
+  // send ICW1 to 4 to PICs
   outb(ICW1, PIC1_COMMAND);
   outb(ICW2_MASTER, PIC1_DATA);
   outb(ICW3_MASTER, PIC1_DATA);
@@ -46,29 +46,37 @@ void i8259_init(void) {
 
 }
 
-/* Enable (unmask) the specified IRQ */
+/* Enable (unmask) the specified IRQ 
+*  This is done by ANDing with the NOT of 1 << irq_num. 
+*  essentially, the old mask is saved
+*/
 void enable_irq(uint32_t irq_num) {
   uint32_t mask = ~(1 << irq_num);
 
-  master_mask &= mask;
-  slave_mask &= mask;
+ // master_mask &= mask;
+  //slave_mask &= mask;
 
-  if(irq_num & 8){
+  if(irq_num >= SIZEOFPIC1){
+    uint32_t unmask = ~(1 << (irq_num - SIZEOFPIC1));
+    slave_mask &= unmask;
     outb(slave_mask, PIC2_DATA);
   }
   else{
+    master_mask &= mask;
     outb(master_mask, PIC1_DATA);
   }
 }
 
-/* Disable (mask) the specified IRQ */
+/* Disable (mask) the specified IRQ 
+*  This is done by ORing with 1 << irq_num
+*/
 void disable_irq(uint32_t irq_num) {
   uint32_t mask = (1 << irq_num);
 
   master_mask |= mask;
   slave_mask |= mask;
 
-  if(irq_num & 8){
+  if(irq_num & SIZEOFPIC1){
     outb(slave_mask, PIC2_DATA);
   }
   else{
@@ -76,10 +84,15 @@ void disable_irq(uint32_t irq_num) {
   }
 }
 
-/* Send end-of-interrupt signal for the specified IRQ */
+/* Send end-of-interrupt signal for the specified IRQ 
+*  We must send EOI | 2 to the master PIC so that it knows 
+*  slave is receiving 
+*/
 void send_eoi(uint32_t irq_num) {
-  if(irq_num >= 8){
-    outb(EOI | irq_num, PIC2_COMMAND);
+  if(irq_num >= SIZEOFPIC1){
+    outb(EOI | (irq_num - SIZEOFPIC1), PIC2_COMMAND);
+    outb(EOI | MSLINK, PIC1_COMMAND);
   }
-  outb(EOI | irq_num, PIC1_COMMAND);
+  else
+    outb(EOI | irq_num, PIC1_COMMAND);
 }
