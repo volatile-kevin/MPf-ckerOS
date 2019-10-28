5be101864d2bb1184e9531431b66e56b4e45e42a
dao3 <dao3@illinois.edu>
2019-10-28 01:57:32 -0500

    Replace keyboard.h

--- student-distrib/keyboard.h
+++ student-distrib/keyboard.h
@@ -2,6 +2,12 @@
 #define KEYBOARD_H
 
 #define STATUS_REGISTER 0x64
+#define MAXCHAR 127
+#define BUFFER_SIZE 128
+// written output buffer
+// The last byte is for 0 which is why there is a + 1
+char buf_kb[BUFFER_SIZE];
+
 
 // initialize keyboard for input
 void init_keyboard();
